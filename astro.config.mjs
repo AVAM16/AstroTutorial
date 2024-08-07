eac13df96a3faba22f836a44e15b8858eee47ae2
AVAM16 <arturvasco.martins@gmail.com>
2024-08-07 17:10:53 +0100

    tutorial finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astrotutorialavam.netlify.app"
+  site: "https://astrotutorialavam.netlify.app",
+  integrations: [preact()]
 });
